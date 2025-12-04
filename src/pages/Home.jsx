ad2b2a311b35a6232c7107c0227ef5760c920941
Shristi Shrivastava <shristi.shrivastava703@gmail.com>
2025-12-04 23:28:12 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 
-import React from 'react';                      
+/*import React from 'react';                      
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
-import Contact from '../components/Contact';
 import Social from '../components/Social';
+import Contact from '../components/Contact';
 import Footer from '../components/Footer';  
 
 
@@ -41,6 +41,70 @@ const Home = () => {
 
         
             <section id="Footer">
+                <Footer/>
+            </section>
+        </>
+    );
+}
+
+export default Home;
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';                      
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Skills from '../components/Skills';
+import Projects from '../components/Projects';
+import Social from '../components/Social';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';  
+
+
+const Home = () => {
+    return (
+        <>
+            <section id="home">
+                <Hero />
+            </section>
+
+            <section id="about">
+                <About />
+            </section>
+
+            <section id="skills">
+                <Skills />
+            </section>
+
+            <section id="projects">
+                <Projects />
+            </section>
+
+            <section id="social">
+                <Social/>
+            </section>
+            
+            <section id="contact">
+                <Contact/>
+            </section>
+
+            <section id="footer">
                 <Footer/>
             </section>
         </>
